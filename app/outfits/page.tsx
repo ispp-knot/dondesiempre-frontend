973cec36dd860e4ab59f59f974b48368526435bb
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2026-02-23 23:48:07 +0100

    fix: reduce width of outfit listing screen

--- app/outfits/page.tsx
+++ app/outfits/page.tsx
@@ -38,57 +38,61 @@ export default function OutfitsPage() {
   } else {
     return (
       <>
-        {testOutfits.map((o) => (
-          <Card key={o.index} className="p-4 m-4">
-            <div>
-              {o.discountedPriceInCents === o.priceInCents ? (
-                <></>
-              ) : (
-                <RiDiscountPercentFill className="text-4xl" />
-              )}
-              <h1 className="mb-3 font-bold text-primary text-center text-3xl">{o.name}</h1>
-              {o.description ? (
-                <p className="text-secondary text-center text-xl">{o.description}</p>
-              ) : (
-                <></>
-              )}
-            </div>
-            <div className="flex flex-row w-fit max-w-11/12 self-center overflow-x-scroll items-center gap-4 p-4">
-              {o.products.map((p) =>
-                p.image ? (
-                  <img
-                    key={p.index}
-                    src={p.image}
-                    alt={'Imagen de producto'}
-                    className="w-30 h-30 md:w-50 md:h-50 object-cover shrink-0 rounded-lg shadow-lg"
-                  ></img>
+        <div className="flex flex-col items-center">
+          <div className="w-full md:w-8/12">
+            {testOutfits.map((o) => (
+              <Card key={o.index} className="p-4 m-4">
+                <div>
+                  {o.discountedPriceInCents === o.priceInCents ? (
+                    <></>
+                  ) : (
+                    <RiDiscountPercentFill className="text-4xl" />
+                  )}
+                  <h1 className="mb-3 font-bold text-primary text-center text-3xl">{o.name}</h1>
+                  {o.description ? (
+                    <p className="text-secondary text-center text-xl">{o.description}</p>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <div className="flex flex-row w-fit max-w-11/12 self-center overflow-x-scroll items-center gap-4 p-4">
+                  {o.products.map((p) =>
+                    p.image ? (
+                      <img
+                        key={p.index}
+                        src={p.image}
+                        alt={'Imagen de producto'}
+                        className="w-30 h-30 md:w-50 md:h-50 object-cover shrink-0 rounded-lg shadow-lg"
+                      ></img>
+                    ) : (
+                      <></>
+                    )
+                  )}
+                </div>
+                {o.discountedPriceInCents === o.priceInCents ? (
+                  <h1 className="font-bold text-primary text-center text-3xl">
+                    {`${convertPrice(o.priceInCents).toFixed(2).toString().replace('.', ',')}€`}
+                  </h1>
                 ) : (
-                  <></>
-                )
-              )}
-            </div>
-            {o.discountedPriceInCents === o.priceInCents ? (
-              <h1 className="font-bold text-primary text-center text-3xl">
-                {`${convertPrice(o.priceInCents).toFixed(2).toString().replace('.', ',')}€`}
-              </h1>
-            ) : (
-              <div className="flex flex-row self-center gap-3">
-                <h1 className="text-primary text-center line-through text-3xl">
-                  {`${convertPrice(o.priceInCents).toFixed(2).toString().replace('.', ',')}€`}
-                </h1>
-                <h1 className="font-bold text-primary text-center text-3xl">
-                  {`${convertPrice(o.discountedPriceInCents).toFixed(2).toString().replace('.', ',')}€`}
-                </h1>
-              </div>
-            )}
-            <Link
-              href={`/outfits/${o.id}`}
-              className="self-center flex flex-wrap items-center justify-center gap-2 md:flex-row rounded-lg bg-secondary hover:bg-dark-secondary hover:cursor-pointer text-white font-bold text-xl h-12 w-11/12 md:w-1/4"
-            >
-              Ver más
-            </Link>
-          </Card>
-        ))}
+                  <div className="flex flex-row self-center gap-3">
+                    <h1 className="text-primary text-center line-through text-3xl">
+                      {`${convertPrice(o.priceInCents).toFixed(2).toString().replace('.', ',')}€`}
+                    </h1>
+                    <h1 className="font-bold text-primary text-center text-3xl">
+                      {`${convertPrice(o.discountedPriceInCents).toFixed(2).toString().replace('.', ',')}€`}
+                    </h1>
+                  </div>
+                )}
+                <Link
+                  href={`/outfits/${o.id}`}
+                  className="self-center flex flex-wrap items-center justify-center gap-2 md:flex-row rounded-lg bg-secondary hover:bg-dark-secondary hover:cursor-pointer text-white font-bold text-xl h-12 w-11/12 md:w-1/4"
+                >
+                  Ver más
+                </Link>
+              </Card>
+            ))}
+          </div>
+        </div>
       </>
     );
   }
