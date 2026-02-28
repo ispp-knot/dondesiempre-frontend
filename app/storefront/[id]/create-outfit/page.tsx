3e2e47aa2fc0173f1525ec6ac25f7133be112920
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2026-02-28 15:26:58 +0100

    fix: fix scrollbar width

--- app/storefront/[id]/create-outfit/page.tsx
+++ app/storefront/[id]/create-outfit/page.tsx
@@ -179,7 +179,7 @@ export default function OutfitProductsPage() {
                         </Button>
                       ))}
                     </div>
-                    <div className="flex flex-row w-full max-w-11/12 self-center overflow-x-scroll items-center gap-4 p-4">
+                    <div className="flex flex-row w-full self-center overflow-x-scroll items-center gap-4 p-4">
                       {outfitProducts.map((p, i) => (
                         <SortableProduct
                           key={p.id}
