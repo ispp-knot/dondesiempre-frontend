b390ebcdd19eddf166022ddd7dc42d8feb5ba4ff
Carlos Cerdá Morales <carcermor@alum.us.es>
2026-03-01 18:57:19 +0100

    feat: outfits correctly displayed but img not working

--- app/stores/[id]/outfits.tsx
+++ app/stores/[id]/outfits.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { Percent } from 'lucide-react';
 import { OutfitDTO } from '@/lib/api/types';
 
@@ -15,27 +16,34 @@ export default function Outfits({ outfits = [] }: Props) {
           Ver más
         </Link>
       </div>
+
       <div className="flex flex-row items-center gap-2 md:gap-4 overflow-x-auto pb-3 store-listing">
         {outfits.map((out) => (
           <div
             key={out.id}
-            className="relative flex flex-col shrink-0 border-2 border-gray-200 w-[45%] md:w-1/4 h-60 sm:h-80 bg-cover bg-center justify-end rounded-lg shadow-sm"
-            style={{
-              backgroundImage: `url(${out.image || `/static/img/outfit_placeholder_${out.id}.jpg`})`,
-            }}
+            className="relative flex flex-col shrink-0 border-2 border-gray-200 w-[45%] md:w-1/4 h-60 sm:h-80 justify-end rounded-lg shadow-sm overflow-hidden bg-gray-100"
           >
-            {out.discountedPriceInCents && (
-              <div className="absolute top-2 left-2 bg-[var(--secondary)] rounded-full p-0.5 md:p-1 flex items-center justify-center shadow-md">
+            <Image
+              src={out.image || '/static/img/banner.jpg'}
+              alt={out.name}
+              fill
+              className="object-cover -z-0"
+              unoptimized
+            />
+
+            {(out.discountedPriceInCents || 0) > 0 && (
+              <div className="absolute top-2 left-2 bg-[var(--secondary)] rounded-full p-0.5 md:p-1 flex items-center justify-center shadow-md z-10">
                 <Percent className="w-4 h-4 md:w-5 md:h-5 text-white stroke-[3]" />
               </div>
             )}
 
             <div
-              className="flex flex-row items-center justify-center gap-1.5 w-full h-4/12 md:h-1/4 self-end bg-white text-sm md:text-lg px-2 text-center"
+              className="relative z-10 flex flex-row items-center justify-center gap-1.5 w-full h-4/12 md:h-1/4 self-end bg-white text-sm md:text-lg px-2 text-center"
               style={{ color: 'var(--primary)' }}
             >
               <span className="truncate">{out.name}</span>
-              {out.discountedPriceInCents && (
+
+              {(out.discountedPriceInCents || 0) > 0 && (
                 <span
                   className="font-bold px-1.5 py-0.5 rounded-md text-xs md:text-sm"
                   style={{
