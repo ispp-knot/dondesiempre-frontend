6f834397d4cf16eb01e936c5c4c390023134a7ef
Emilio-115 <emivazcru@alum.us.es>
2026-03-03 17:21:24 +0100

    feat: :sparkles: Add dialog component, types for promote items and placeholder

--- lib/api/types.tsx
+++ lib/api/types.tsx
@@ -26,3 +26,52 @@ export type StoreDTO = {
   storefront: StorefrontDTO;
   socialNetworks: StoreSocialNetworkDTO[];
 };
+
+export interface ProductType {
+  id: string;
+  type: string;
+}
+
+export interface OutfitProductDTO {
+  id: string;
+  name: string;
+  description: string;
+  priceInCents: number;
+  type: ProductType;
+  index: number;
+  storeId: string;
+}
+
+export interface OutfitDTO {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  priceInCents: number;
+  discountedPriceInCents: number;
+  index: number;
+  storefrontId: number;
+  tags: string[];
+  products: OutfitProductDTO[];
+}
+
+export interface ProductDTO {
+  name: string;
+  priceInCents: number;
+  image: string;
+  discountedPriceInCents: number;
+  description: string;
+  typeId: string;
+  storeId: string;
+}
+
+export interface Promotion {
+  id: string;
+  name: string;
+  discountPercentage: number;
+  isActive: boolean;
+  description: string;
+  image: string | null;
+  storeId: string;
+  productIds: string[];
+}
