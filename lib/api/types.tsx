734900656f21737f9c6666b43547d4072ed15f83
luna3 <loufouela@alum.us.es>
2026-03-03 19:11:15 +0100

    fix: fixed merge conflicts

--- lib/api/types.tsx
+++ lib/api/types.tsx
@@ -28,32 +28,5 @@ export type StoreDTO = {
   primaryColor: string;
   secondaryColor: string;
   bannerImageUrl: string;
-  socialNetworks?: SocialNetworkDTO[];
-};
-
-export type OutfitProductDTO = {
-  id: string;
-  name: string;
-  description: string;
-  image: string;
-  priceInCents: number;
-  type: string;
-  index: number;
-};
-
-export type OutfitDTO = {
-  id: string;
-  name: string;
-  description: string;
-  image: string;
-  priceInCents: number;
-  discountedPriceInCents: number;
-  index: number;
-  tags: string[];
-  products: string[];
-};
-
-export type SocialNetworkDTO = {
-  name: string;
-  link: string;
+  socialNetworks?: StoreSocialNetworkDTO[];
 };
