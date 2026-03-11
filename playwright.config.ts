d09eb1d8726070165cb8fbc23a47192508ca245c
miniluz <javiermelon4fu@gmail.com>
2026-03-11 09:59:37 +0100

    fix: 🔥 Remove dead CI code, since we're not doing CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import { DEFAULT_MAP_LOCATION } from './lib/mapUtils';
 
-const isCI = !!process.env.CI;
-
-// 2. Definimos la ruta: en CI está dentro, en local está "al lado" (..)
-
-// 3. Definimos el COMANDO basándonos SOLO en el Sistema Operativo
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// We expect the backend to already be running
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,11 +11,7 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -40,16 +25,6 @@ export default defineConfig({
       // stdout: 'pipe', // <--- Verás los logs del Frontend
       // stderr: 'pipe',
     },
-    ...(isCI
-      ? [
-          {
-            command: `cd dondesiempre-backend && ./mvnw spring-boot:run "-Dspring-boot.run.profiles=dev-migration,seed" && ./mvnw spring-boot:run "-Dspring-boot.run.profiles=dev-migration"`,
-            url: 'http://localhost:8080/api/v1/health',
-            reuseExistingServer: false, // En CI queremos que lo levante siempre de cero
-            timeout: 300 * 1000,
-          },
-        ]
-      : []),
   ],
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
