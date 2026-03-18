7b39f6735fb663e78edf07c730780e948a738451
Emilio-115 <emiliovc2016@gmail.com>
2026-03-18 12:00:43 +0100

    feat: :sparkles: add clickable button to move and extract component

--- app/stores/[id]/collections.tsx
+++ app/stores/[id]/collections.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+'use client';
+import HorizontalScroll from '@/components/dondeSiempre/HorizontalScroll';
 
 type Collection = {
   id: number;
@@ -11,32 +12,23 @@ type Props = {
   collections?: Collection[];
 };
 
-export default function Collections({ storeId = undefined, collections = [] }: Props) {
+// collections.tsx
+export default function Collections({ storeId, collections = [] }: Props) {
   return (
-    <div className="flex flex-col px-5 sm:w-10/12">
-      <div className="flex flex-row items-center justify-between w-full mb-4">
-        <h1 className="text-primary text-xl md:text-2xl font-bold">Nuestras colecciones</h1>
-        <Link href={`/stores/${storeId}/collections`} className="text-secondary underline">
-          Ver más
-        </Link>
-      </div>
-      <div className="flex flex-row items-center gap-2 md:gap-4 overflow-x-auto pb-3 storefront-listing">
-        {collections.map((col) => (
-          <div
-            key={col.id}
-            className="flex flex-col shrink-0 border-2 border-gray-200 w-[45%] md:w-1/4 h-40 sm:h-60 bg-cover bg-center justify-end rounded-lg shadow-sm"
-            style={{
-              backgroundImage: `url(${col.image || `/static/img/collection_placeholder_${col.id}.jpg`})`,
-            }}
-          >
-            <div className="flex flex-col items-center justify-center gap-2 w-full h-4/12 md:h-1/4 self-end bg-white text-sm sm:text-lg px-4 text-center">
-              {col.name}
-            </div>
+    <HorizontalScroll title="Nuestras colecciones" viewMoreHref={`/stores/${storeId}/collections`}>
+      {collections.map((col) => (
+        <div
+          key={col.id}
+          className="flex flex-col shrink-0 border-2 border-gray-200 w-[45%] md:w-1/4 h-40 sm:h-60 bg-cover bg-center justify-end rounded-lg shadow-sm"
+          style={{
+            backgroundImage: `url(${col.image || `/static/img/collection_placeholder_${col.id}.jpg`})`,
+          }}
+        >
+          <div className="flex flex-col items-center justify-center gap-2 w-full h-4/12 md:h-1/4 self-end bg-white text-sm sm:text-lg px-4 text-center">
+            {col.name}
           </div>
-        ))}
-      </div>
-      {/* style={{ backgroundImage: `url(${col.image})` }} */}
-      {/* TODO: Style collections with images */}
-    </div>
+        </div>
+      ))}
+    </HorizontalScroll>
   );
 }
