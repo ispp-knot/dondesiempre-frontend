08b82619fe2d231fb2c3240cd0e9f71478e6f7eb
Emilio-115 <emiliovc2016@gmail.com>
2026-03-18 16:12:34 +0100

    fix: :rotating_light: correct problems with props

--- app/stores/[id]/outfits/[outfitId]/products/page.tsx
+++ app/stores/[id]/outfits/[outfitId]/products/page.tsx
@@ -142,7 +142,7 @@ export default function OutfitProductsPage() {
                       ></Image>
                     </div>
                     <h1 className="font-bold text-primary text-center text-lg md:text-2xl">
-                      {`${convertPrice(p.discountedPriceInCents).toFixed(2).toString().replace('.', ',')}€`}
+                      {`${convertPrice(p.priceInCents).toFixed(2).toString().replace('.', ',')}€`}
                     </h1>
                     <div className="flex flex-row justify-center">
                       <Button
