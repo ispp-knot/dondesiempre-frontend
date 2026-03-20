1fb53e1eb93e51d96d1483226ac70b2153d0d078
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2026-03-20 22:07:33 +0100

    fix: fix incorrect outfit price display

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,9 @@ export function cn(...inputs: ClassValue[]) {
 export function convertPrice(priceInCents: number): number {
   return priceInCents / 100;
 }
+
+export function discountPrice(priceInCents: number, discountPercentage: number | null): number {
+  return discountPercentage
+    ? convertPrice(priceInCents * ((100 - discountPercentage) / 100))
+    : convertPrice(priceInCents);
+}
