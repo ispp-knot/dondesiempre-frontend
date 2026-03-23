9972a0d6f8ea268d3e790b1b314eb4e5e876eeb1
Miguel <miguelgvizcaino@gmail.com>
2026-03-23 20:59:34 +0100

    feat: :zap: fix discount

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,11 +6,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function convertPrice(priceInCents: number): number {
-  return Math.round(priceInCents / 100);
+  return Number((priceInCents / 100).toFixed(2));
 }
 
 export function discountPrice(priceInCents: number, discountPercentage: number | null): number {
   return discountPercentage
-    ? Math.round(convertPrice(priceInCents * ((100 - discountPercentage) / 100)))
-    : Math.round(convertPrice(priceInCents));
+    ? (convertPrice(priceInCents * ((100 - discountPercentage) / 100)))
+    : (convertPrice(priceInCents));
 }
