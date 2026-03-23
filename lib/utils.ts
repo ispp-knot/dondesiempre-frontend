95bb7fb8ac4aa7c778e8b5c96d78a74ddbcbb6fc
Miguel <miguelgvizcaino@gmail.com>
2026-03-23 20:47:01 +0100

    feat: :zap: adding round to discountPrice and convertPrice

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,11 +6,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function convertPrice(priceInCents: number): number {
-  return priceInCents / 100;
+  return Math.round(priceInCents / 100);
 }
 
 export function discountPrice(priceInCents: number, discountPercentage: number | null): number {
   return discountPercentage
-    ? convertPrice(priceInCents * ((100 - discountPercentage) / 100))
-    : convertPrice(priceInCents);
+    ? Math.round(convertPrice(priceInCents * ((100 - discountPercentage) / 100)))
+    : Math.round(convertPrice(priceInCents));
 }
