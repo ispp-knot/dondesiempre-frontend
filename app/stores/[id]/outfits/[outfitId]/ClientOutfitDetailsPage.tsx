a08e53bee9cb508fcd77f718c43a5c50b71ff632
miniluz <javiermelon4fu@gmail.com>
2026-03-24 09:13:29 +0100

    fix: 🚨 Fix linter

--- app/stores/[id]/outfits/[outfitId]/ClientOutfitDetailsPage.tsx
+++ app/stores/[id]/outfits/[outfitId]/ClientOutfitDetailsPage.tsx
@@ -140,7 +140,13 @@ export default function ClientOutfitDetailsPage(props: ClientOutfitDetailsPagePr
               <div>
                 <h1 className="mt-4 mb-4 text-primary text-2xl">
                   <strong>Total: </strong>
-                  {`${discountPrice(props.outfit.priceInCents, props.outfit.discountPercentage).toFixed(2).toString().replace('.', ',')}€ con IVA`}
+                  {`${discountPrice(
+                    props.outfit.priceInCents,
+                    props.outfit.discountPercentage ?? null
+                  )
+                    .toFixed(2)
+                    .toString()
+                    .replace('.', ',')}€ con IVA`}
                 </h1>
               </div>
               <Button
@@ -163,7 +169,13 @@ export default function ClientOutfitDetailsPage(props: ClientOutfitDetailsPagePr
             {props.outfit && (
               <p className="text-secondary text-center">
                 Vas a realizar un pedido por un total de{' '}
-                <strong>{`${discountPrice(props.outfit.priceInCents, props.outfit.discountPercentage).toFixed(2).toString().replace('.', ',')}€`}</strong>
+                <strong>{`${discountPrice(
+                  props.outfit.priceInCents,
+                  props.outfit.discountPercentage ?? null
+                )
+                  .toFixed(2)
+                  .toString()
+                  .replace('.', ',')}€`}</strong>
                 .
               </p>
             )}
