ea35ae41644bdf283de1b4de37eee55affd41fbd
Miguel <miguelgvizcaino@gmail.com>
2026-03-24 13:03:30 +0100

    feat: :bug: removing previous promotion panel

--- app/stores/[id]/store-tabs.tsx
+++ app/stores/[id]/store-tabs.tsx
@@ -236,28 +236,6 @@ export default function StoreTabs({
           )}
         </div>
       )}
-      {promoOutfit && (
-        <div className="relative mx-4 mt-5 flex flex-col items-center justify-center border-2 border-secondary/50 rounded-md p-4 overflow-hidden w-11/12 sm:w-3/4 lg:max-w-3xl sm:mx-auto">
-          <div className="absolute inset-0 z-0 w-full h-full">
-            {promoOutfit.image && (
-              <Image src={promoOutfit.image} alt={promoOutfit.name} fill className="object-cover" />
-            )}
-            <div className="absolute inset-0 bg-white/85"></div>
-          </div>
-
-          <div className="relative z-10 flex flex-col items-center w-full">
-            <h3 className="text-primary font-bold text-lg md:text-xl">¡En promoción!</h3>
-            <h2 className="text-secondary font-bold text-4xl md:text-5xl mt-1 text-center">
-              {promoOutfit.name}
-            </h2>
-            <p className="text-primary font-semibold mt-1">12/04/2026 - 26/04/2026</p>
-            <ShareTo
-              item={promoOutfit}
-              className="bg-secondary text-white font-medium py-2 px-4 rounded mt-4 w-fit shadow-sm hover:bg-secondary/90 hover:cursor-pointer transition"
-            />
-          </div>
-        </div>
-      )}
 
       <div className="flex mx-4 mt-5 mb-5 self-center rounded-md overflow-hidden border border-gray-200 w-11/12 sm:mx-auto sm:max-w-142.5">
         <button
