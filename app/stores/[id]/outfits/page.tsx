ba231ecfd69e012c18e7c2adfe16c378a7589804
Miguel <miguelgvizcaino@gmail.com>
2026-05-04 09:51:32 +0200

    fix: :bug: fixing an error in wich if you press elsewhere in the button of create outfit that wasnt the text it doesnt work

--- app/stores/[id]/outfits/page.tsx
+++ app/stores/[id]/outfits/page.tsx
@@ -141,16 +141,14 @@ export default function OutfitsPage() {
               </Card>
             )}
             <div className="flex flex-col sm:flex-row justify-between w-full gap-2 p-4">
-              <div className="self-center flex flex-wrap items-center justify-center gap-2 md:flex-row rounded-lg bg-secondary hover:bg-dark-secondary hover:cursor-pointer text-white font-bold text-md md:text-xl w-full h-12">
-                <Link
+              <Link
                   href={`/stores/${params.id}/create-outfit/`}
-                  className="flex flex-row gap-2"
+                  className="self-center flex flex-wrap items-center justify-center gap-2 md:flex-row rounded-lg bg-secondary hover:bg-dark-secondary hover:cursor-pointer text-white font-bold text-md md:text-xl w-full h-12"
                   data-testid="create-outfit-button"
                 >
                   <IoMdAddCircleOutline className="mt-0.5 text-white text-center text-2xl" />
                   <h1 className="font-bold text-white text-center text-xl">Crear outfit</h1>
                 </Link>
-              </div>
               {isOrdering ? (
                 <div
                   className="p-2 self-center flex flex-wrap items-center justify-center gap-2 md:flex-row rounded-lg bg-primary hover:bg-dark-primary hover:cursor-pointer text-white font-bold text-md md:text-xl w-full h-12"
